755f2c39adb3be199666d7590803a743356f0e39
Sergio Terroso <sergio.terroso@email.com>
2024-05-21 11:52:15 -0600

    Modules & Imports lesson

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"example.com/blueprint/modules"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	myTicket := modules.Ticket{
+		ID:    123,
+		Event: "A multicultural event",
+	}
+
+	myTicket.PrintEvent()
 }
